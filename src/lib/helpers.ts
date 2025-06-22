ecf1ba2b62597d1510441a71d0648578cccff16d
Jourdan Totonde <108494290+coddano@users.noreply.github.com>
2025-06-23 00:10:45 +0100

    Switch time zone display from Italy to GMT
    Updated TimeZoneCard component and helper functions to display and format time in GMT instead of Italy's time zone. This includes renaming and refactoring helper functions and updating all relevant imports and usages.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,32 +2,22 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
-  // Create a date object with the current UTC time
-  const now = new Date();
 
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
-
-  return now;
+export function getCurrentTimeInGMT(): Date {
+  return new Date();
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTimeForGMT(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
-    hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    hour12: true,
+    timeZone: "GMT",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
-
-  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  formattedTime += " GMT";
 
   return formattedTime;
 }
-``;
